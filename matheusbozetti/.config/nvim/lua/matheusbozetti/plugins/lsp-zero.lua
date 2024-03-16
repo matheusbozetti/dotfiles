53f99bc3559501a9dac667f59589d71bbe8fc9b6
Matheus Bozetti <matheusbozetti@MacBook-Pro-de-Matheus.local>
2024-03-16 16:52:29 -0400

    fix autosuggestions

--- matheusbozetti/.config/nvim/lua/matheusbozetti/plugins/lsp-zero.lua
+++ matheusbozetti/.config/nvim/lua/matheusbozetti/plugins/lsp-zero.lua
@@ -10,8 +10,8 @@ return {
 
     -- Autocompletion
     { 'hrsh7th/nvim-cmp' }, -- Required
-    { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-    { 'L3MON4D3/LuaSnip' }, -- Required
+    { 'hrsh8th/cmp-nvim-lsp' }, -- Required
+    { 'L4MON4D3/LuaSnip' }, -- Required
   },
   config = function()
     local lsp_zero = require('lsp-zero')
@@ -62,6 +62,58 @@ return {
       },
       opts = {
         auto_install = true,
+      },
+    })
+
+    -- TODO: Move to another file and test it
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+    luasnip.config.setup({})
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = { completeopt = 'menu,menuone,noinsert' },
+      mapping = cmp.mapping.preset.insert({
+        -- Select the [n]ext item
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        -- Select the [p]revious item
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+        -- Accept ([y]es) the completion.
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        --  completions whenever it has completion options available.
+        ['<C-Space>'] = cmp.mapping.complete({}),
+        ['<C-0>'] = cmp.mapping.complete({}),
+        -- <c-l> will move you to the right of each of the expansion locations.
+        -- <c-h> is similar, except moving you backwards.
+        ['<C-l>'] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { 'i', 's' }),
+        ['<C-h>'] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { 'i', 's' }),
+      }),
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip', max_item_count = 5 },
+        { name = 'buffer', max_item_count = 5 },
+        { name = 'codeium.nvim', max_item_count = 5 },
+        { name = 'path', max_item_count = 5 },
+      },
+      experimental = {
+        ghost_text = true,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
       },
     })
   end,
