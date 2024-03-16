ddee90de59a58888758001a2e21909b6e17abae7
Matheus Bozetti <matheusbozetti@MBP-de-Matheus.local>
2024-03-16 14:40:00 -0400

    add todo-comments plugin

--- matheusbozetti/.config/nvim/lua/matheusbozetti/plugins/todo-comments.lua
+++ matheusbozetti/.config/nvim/lua/matheusbozetti/plugins/todo-comments.lua
@@ -2,5 +2,20 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false },
+  config = function()
+    require('todo-comments').setup({
+      signs = false,
+    })
+
+    vim.keymap.set('n', ']t', function()
+      require('todo-comments').jump_next()
+    end, { desc = 'Next todo comment' })
+
+    vim.keymap.set('n', '[t', function()
+      require('todo-comments').jump_prev()
+    end, { desc = 'Previous todo comment' })
+
+    vim.keymap.set('n', '<leader>ptt', '<cmd>:TodoTelescope<CR>', { desc = '[T]odo comments [T]elescope' })
+    vim.keymap.set('n', '<leader>ptl', '<cmd>:TodoLocList<CR>', { desc = '[T]odo coments [L]ocList' })
+  end,
 }
