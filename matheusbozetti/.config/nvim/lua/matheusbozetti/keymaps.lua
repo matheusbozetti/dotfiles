cea815b6481ba7e400a91114bb5e857f97c08cf5
Matheus Bozetti <matheusbozetti@MacBook-Pro-de-Matheus.local>
2024-03-27 16:23:59 -0400

    update nvim keymaps

--- matheusbozetti/.config/nvim/lua/matheusbozetti/keymaps.lua
+++ matheusbozetti/.config/nvim/lua/matheusbozetti/keymaps.lua
@@ -8,10 +8,14 @@ vimSetKeymap('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open vertical NetEW' })
 vimSetKeymap('n', '<C-d>', '<C-d>zz', { desc = 'Move down and center' })
 vimSetKeymap('n', '<C-u>', '<C-u>zz', { desc = 'Move up and center' })
 
+vimSetKeymap('n', '<M-S-j>', '5j', { desc = 'Move down 5 lines' })
+vimSetKeymap('n', '<M-S-k>', '5k', { desc = 'Move up 5 lines' })
+
 vimSetKeymap('n', '<leader>p', '"_dP', { desc = 'Change the highlighted text with the previous copied' })
 
 vimSetKeymap('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Search and replace' })
 
+vimSetKeymap('n', '<C-s>', ':w<CR>', { desc = 'Save buffer' })
 vimSetKeymap('n', '<C-l>', ':bnext<CR>', { desc = 'Next buffer' })
 vimSetKeymap('n', '<C-h>', ':bprev<CR>', { desc = 'Previous buffer' })
 vimSetKeymap('n', '<leader>q', ':bd<CR>', { desc = 'Close current buffer' })
