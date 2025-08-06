9116a64fed179ae4c738b1d6b2179194d5a02aad
Matheus Bozetti <matheusbzt@gmail.com>
2025-08-06 09:10:45 -0400

    ✨ update nvim files

--- matheusbozetti/.config/nvim/init.lua
+++ matheusbozetti/.config/nvim/init.lua
@@ -1,19 +1,12 @@
-require('matheusbozetti')
+require("core")
 
--- [[ Install `lazy.nvim` plugin manager ]]
-
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({
-  { import = 'matheusbozetti.plugins' },
+-- Treesitter
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "<filetype>" },
+	callback = function()
+		vim.treesitter.start()
+	end,
 })
 
--- Applies the colorscheme and transparency
--- lua/matheusbozetti/colors.lua
-ColorMe('gruvbox-material')
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
